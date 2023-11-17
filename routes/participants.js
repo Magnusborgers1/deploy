e44cedabc397968963ced4208fb37203622376f2
Magnus Borgersen <magnusborgersen@gmail.com>
2023-11-17 21:29:43 +0100

    started to implement db

--- routes/participants.js
+++ routes/participants.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { dynamoDB, tableName } = require('../db');
+// const { dynamoDB, tableName } = require('../db');
 const authenticateAdmin = require('../authenticate');
+const CyclicDB = require('@cyclic.sh/dynamodb');
+const db = CyclicDB(process.env.CYCLIC_DB);
+let participants = db.collection('participants');
 
 // Endpoint to add participant
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
   console.log('Entered participants/add post call');
   const participantData = req.body;
   const { email, firstname, lastname, dob, active, work, home } = participantData;
-
+let list = await participants.list();
   if (!email || !firstname || !lastname || !dob || !active || !work || !home) {
     return res.status(400).json({ error: 'Incomplete participant data. All fields are required.' });
   }
 
-  const params = {
-    TableName: tableName,
-    Item: participantData,
-  };
+  await participants.set(email, {
+    firstName: firstname,
+    secondName: lastname,
+    dob: dob
+  });
+  return res.end();
 
   dynamoDB.put(params, (error) => {
     if (error) {
@@ -24,7 +29,7 @@ router.post('/add', (req, res) => {
       return res.status(500).json({ error: 'Failed to insert participant into the database.' });
     }
 
-    res.json({ message: 'Participant added successfully' });
+    return res.json({ message: 'Participant added successfully' });
   });
 });
 
@@ -44,6 +49,78 @@ router.get('/', (req, res) => {
     res.json({ participants });
   });
 });
+
+
+// Endpoint to delete participant by email
+router.delete('/:email', authenticateAdmin, (req, res) => {
+  const emailToDelete = req.params.email;
+
+  // Validate email
+  if (!validateEmail(emailToDelete)) {
+    return res.status(400).json({ error: 'Invalid email format.' });
+  }
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      email: emailToDelete,
+    },
+  };
+
+  dynamoDB.delete(params, (error) => {
+    if (error) {
+      console.error('Error deleting participant from the database:', error);
+      return res.status(500).json({ error: 'Failed to delete participant from the database.' });
+    }
+
+    res.json({ message: 'Participant deleted successfully' });
+  });
+});
+
+
+router.put('/:email', authenticateAdmin, (req, res) => {
+  const emailToUpdate = req.params.email;
+  const updatedParticipantData = req.body;
+
+  // Validate email format (kanskje forbedre?)
+  if (!validateEmail(emailToUpdate)) {
+    return res.status(400).json({ error: 'Invalid email format.' });
+  }
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      email: emailToUpdate,
+    },
+    UpdateExpression: 'SET firstname = :firstname, lastname = :lastname, dob = :dob, active = :active, work = :work, home = :home',
+    ExpressionAttributeValues: {
+      ':firstname': updatedParticipantData.firstname,
+      ':lastname': updatedParticipantData.lastname,
+      ':dob': updatedParticipantData.dob,
+      ':active': updatedParticipantData.active,
+      ':work': updatedParticipantData.work,
+      ':home': updatedParticipantData.home,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  dynamoDB.update(params, (error, data) => {
+    if (error) {
+      console.error('Error updating participant in the database:', error);
+      return res.status(500).json({ error: 'Failed to update participant in the database.' });
+    }
+
+    res.json({ message: 'Participant updated successfully', updatedParticipant: data.Attributes });
+  });
+});
+
+
+// Function to validate email format (kanskje forbedre?)
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
 
 
 console.log('added participant router');
